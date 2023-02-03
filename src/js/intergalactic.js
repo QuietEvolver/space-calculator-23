162e5d003addf940a18cbe3eb1c24e37e88d33e1
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 14:55:33 -0800

    WIP: Definition - Adds getYearsSinceBirthdayAge()fxn on JS.

--- src/js/intergalactic.js
+++ src/js/intergalactic.js
@@ -1,17 +1,3 @@
-/*
-### 2 ##### Determines how many years have passed on each planet since a past birthday. For example, if a user is 56 and we want to calculate how many years have passed since their 43 birthday, we would find the following results:
-13 Earth years have passed.
-54.16 Mercury years have passed.
-8.06 Venus years have passed.
-6.91 Mars years have passed.
-1.09 Jupiter years have passed.
-
-### 3 ##### Determines how many years have yet to pass until a future birthday. For example, if a user is 56 years and we want to calculate how many years have yet to pass until they are 61 years old, we would find the following results:
-5 Earth years have yet to pass.
-20.83 Mercury years have yet to pass.
-20.96 Venus years have yet to pass.
-2.65 Mars years have yet to pass.
-0.42 Jupiter years have yet to pass. */
 
 /* ### 1 #######
 BI takes a person's age in years and create a class that does the following:
@@ -19,7 +5,7 @@ BI takes a person's age in years and create a class that does the following:
 Returns their age in Mercury years. (A Mercury year is .24 Earth years.)
 Returns their age in Venus years. (A Venus year is .62 Earth years.)
 Returns their age in Mars years. (A Mars year is 1.88 Earth years.)
-Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)*/
+Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)
 
 // for each planet in plantes, do this. 
 // Map.
@@ -34,6 +20,21 @@ Returns their age in Jupiter years. (A Jupiter year is 11.86 Earth years.)*/
 // )
 
 // iCalculator.entries()
+
+### 2 ##### Determines how many years have passed on each planet since a past birthday. For example, if a user is 56 and we want to calculate how many years have passed since their 43 birthday, we would find the following results:
+13 Earth years have passed.
+54.16 Mercury years have passed.
+8.06 Venus years have passed.
+6.91 Mars years have passed.
+1.09 Jupiter years have passed.
+
+### 3 ##### Determines how many years have yet to pass until a future birthday. For example, if a user is 56 years and we want to calculate how many years have yet to pass until they are 61 years old, we would find the following results:
+5 Earth years have yet to pass.
+20.83 Mercury years have yet to pass.
+20.96 Venus years have yet to pass.
+2.65 Mars years have yet to pass.
+0.42 Jupiter years have yet to pass. */
+
 
 export default class Calculator{
   constructor ( birthdayAge) {
@@ -80,17 +81,17 @@ export default class Calculator{
   // Map.
 
 
-  getYearsSinceBirthdayAge(){
-  // let iCalculator = new Map(
-  //   [
-  //     ["Earth", 1],
-  //     ["Mercury", .24],
-  //     ["Venus", .62],
-  //     ["Mars", 1.88], 
-  //     ["Jupiter", 11.86]
-  //   ]
-  // )
-
+  getYearsSinceBirthdayAge(planetName){
+    const iCalculator = new Map(
+    [
+      ["Earth", 1],
+      ["Mercury", .24],
+      ["Venus", .62],
+      ["Mars", 1.88], 
+      ["Jupiter", 11.86]
+    ]
+  );
+ return Number(this.birthdayAge - iCalculator.entries(planetName));
   // iCalculator.entries()
   }
 }
