09f39086c014b436eba5a8e04bcd22fda7bfa9aa
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 10:45:34 -0800

    Add test valdiation for givenAgeValidator() at 0.

--- __tests__/intergalactic.test.js
+++ __tests__/intergalactic.test.js
@@ -2,8 +2,13 @@ import Calculator from "../src/js/intergalactic";
 
 describe('Calculator', () => {
   
-  test('Should return an age given an inputted age', () => {
+  test('Should hold an age given an inputted age', () => {
     const calculator =  new Calculator(92);
     expect(calculator.birthdayAge).toEqual(92);
+  });
+
+  test('Should account for validation of an age given an inputted age', () => {
+    const calculator =  new Calculator(0);
+    expect(calculator.birthdayAge).toEqual("undefined");
   });
 });
