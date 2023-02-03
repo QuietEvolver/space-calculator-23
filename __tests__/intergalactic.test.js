84560703b4db750c6599ee0dbb8c4332dd642ae1
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 11:03:03 -0800

    Add test fxn method givenAgeValidation().

--- __tests__/intergalactic.test.js
+++ __tests__/intergalactic.test.js
@@ -9,6 +9,13 @@ describe('Calculator', () => {
 
   test('Should account for validation of an age given an inputted age', () => {
     const calculator =  new Calculator(0);
-    expect(calculator.birthdayAge).toEqual("undefined");
-  });
+    expect(calculator.birthdayAge).toEqual(0);
+  }); 
+
+  // //if undef
+  // test('Should account for validation of an age given an inputted age', () => {
+  //   const calculator =  new Calculator("undefined");
+  //   expect(calculator.birthdayAge).toEqual(0);
+  // });
+  
 });
