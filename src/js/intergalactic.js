a2fe884c86d27c274d006a71730610903a2e112b
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 09:41:46 -0800

    Adding planet consts.

--- src/js/intergalactic.js
+++ src/js/intergalactic.js
@@ -41,5 +41,15 @@ export default class Calculator{
 
 
 }
-
-export const mercuryDaysInYear = .24;
+//Years
+export const mercuryYear = .24;
+export const venusYear = .62;
+export const marsYear = 1.88;
+export const jupiterYear = 11.86;
+export const earthYear = 1;
+//Days
+export const mercuryDaysInYear = 88;
+export const venusDaysInYear =  243;
+export const marsDaysInYear = 320;
+export const jupiterDaysInYear = 4332.59;
+export const earthDaysInYear = 365.25;
