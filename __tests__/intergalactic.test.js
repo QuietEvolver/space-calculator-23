74403595d31d774479adec0e3aa01ec6c9248d4e
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 11:27:53 -0800

    WIP: Add else test & line cvg for fxn method givenAgeValidation().

--- __tests__/intergalactic.test.js
+++ __tests__/intergalactic.test.js
@@ -8,14 +8,14 @@ describe('Calculator', () => {
   });
 
   test('Should account for validation of an age given an inputted age', () => {
-    const calculator =  new Calculator(1);
-    expect(calculator.birthdayAge).toEqual(1);
+    const calculator =  new Calculator(92);
+    expect(calculator.birthdayAge).toEqual(92);
   }); 
 
   //if undef
-  test('Should account for validation of an age given an inputted age', () => {
-    const calculator =  new Calculator(5);
-    expect(calculator.birthdayAge).toEqual(5);
+  test('Should test validation of an age given an inputted age', () => {
+    const calculator =  new Calculator(undefined);
+    expect(calculator.birthdayAge).toEqual(0);
   });
   
 });
