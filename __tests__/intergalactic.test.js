b1aeb389a5e304c8399f10be90a595a4e18ab037
Vera Weikel <vera.weikel@gmail.com>
2023-02-03 16:11:13 -0800

    Test re-added NaN eval in validator fxn.

--- __tests__/intergalactic.test.js
+++ __tests__/intergalactic.test.js
@@ -13,30 +13,35 @@ describe('Calculator', () => {
   }); 
 
   // if undef
-  // test('Should test validation of an age given an inputted age to return 0 if not inputted', () => {
-  //   const calculator =  new Calculator(0);
-  //   expect(calculator.givenAgeValidation()).toEqual(0);
-  // });
+  test('Should test validation of an age given an inputted age is not an integer', () => {
+    const calculator =  new Calculator(0);
+    expect(calculator.givenAgeValidation()).toEqual(0);
+  });
+  // if NaN
+  test('Should test validation of an age given an inputted age is not an integer', () => {
+    const calculator =  new Calculator("%");
+    expect(calculator.givenAgeValidation()).toEqual(NaN);
+  });
 
-    test('Should test Mercury age given an inputted age', () => {
-      const calculator =  new Calculator(4);
-      expect(calculator.mercuryAge()).toEqual(0.96);
-    });
-  
-    test('Should test Venus age given an inputted age', () => {
-      const calculator =  new Calculator(4);
-      expect(calculator.venusAge()).toEqual(2.48);
-    });
-  
-    test('Should test Mars age given an inputted age', () => {
-      const calculator =  new Calculator(4);
-      expect(calculator.marsAge()).toEqual(7.52);
-    });
+  test('Should test Mercury age given an inputted age', () => {
+    const calculator =  new Calculator(4);
+    expect(calculator.mercuryAge()).toEqual(0.96);
+  });
 
-    test('Should test Jupiter age given an inputted age', () => {
-      const calculator =  new Calculator(4);
-      expect(calculator.jupiterAge()).toEqual(47.44);
-    });
+  test('Should test Venus age given an inputted age', () => {
+    const calculator =  new Calculator(4);
+    expect(calculator.venusAge()).toEqual(2.48);
+  });
+
+  test('Should test Mars age given an inputted age', () => {
+    const calculator =  new Calculator(4);
+    expect(calculator.marsAge()).toEqual(7.52);
+  });
+
+  test('Should test Jupiter age given an inputted age', () => {
+    const calculator =  new Calculator(4);
+    expect(calculator.jupiterAge()).toEqual(47.44);
+  });
 //undef
     // test('Should test one age to the next age given an inputted age', () => {
     //   const calculator =  new Calculator("Mercury");
