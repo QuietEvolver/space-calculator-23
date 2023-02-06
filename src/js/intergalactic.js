2f6e8b81b201481a8f0528d2b66178d7bafaa606
Vera Weikel <vera.weikel@gmail.com>
2023-02-06 04:51:24 -0800

    Adds or stmt to if on JS.

--- src/js/intergalactic.js
+++ src/js/intergalactic.js
@@ -27,38 +27,40 @@ export default class Calculator{
     this.birthdayAge = birthdayAge;
   }
 
-  givenAgeValidation(){//if(this.birthdayAge !== 0)  
+  givenAgeValidation(){ 
+    if((isNaN(this.birthdayAge) || this.birthdayAge < 0 || undefined)){
+      console.log(this.birthdayAge, "This bdayAge");
+      return this.birthdayAge = 0;
+    } 
+      // return this.birthdayAge;
+    
     // if (this.birthdayAge === undefined){
     //   return this.birthdayAge = 0;
     // }
-    if(this.birthdayAge !== 0){
-      return this.birthdayAge;
-    }
+    // if(this.birthdayAge !== 0){
+    //   return this.birthdayAge;
+    // }
     // if (isNaN(this.birthdayAge))
     // {
     //   return NaN;
     // }
   }
 
-  //     ["Mercury", .24]
   mercuryAge(){
     const birthdayAge = this.birthdayAge;
     return birthdayAge * .24;
   }
 
-  // ["Venus", .62]
   venusAge(){
     const birthdayAge = this.birthdayAge;
     return birthdayAge * .62;
   }
 
-  //     ["Mars", 1.88]
   marsAge(){
     const birthdayAge = this.birthdayAge;
     return birthdayAge * 1.88;
   }
 
-  //     ["Jupiter", 11.86]
   jupiterAge(){
     const birthdayAge = this.birthdayAge;
     return birthdayAge * 11.86;
@@ -68,8 +70,7 @@ export default class Calculator{
   // for each planet in plantes, do this. 
   // Map.
 
-
-  getYearsSinceBirthdayAge(){//planetName
+  // getYearsSinceBirthdayAge(){//planetName
   //   let birthdayAge = this.birthdayAge;
   //   const iCalculator = new Map(
   //   [
@@ -84,18 +85,5 @@ export default class Calculator{
   //   return Number(birthdayAge - iCalculator.entries(planetName));
   // }
   // // iCalculator.entries()
-  }
+  // }
 }
-
-// //Years
-// export const mercuryYear = .24;
-// export const venusYear = .62;
-// export const marsYear = 1.88;
-// export const jupiterYear = 11.86;
-// export const earthYear = 1;
-// //Days
-// export const mercuryDaysInYear = 88;
-// export const venusDaysInYear =  243;
-// export const marsDaysInYear = 320;
-// export const jupiterDaysInYear = 4332.59;
-// export const earthDaysInYear = 365.25;
