e6de3b8fa76c86defb799fc97faa5b801c1fe33d
Vera Weikel <vera.weikel@gmail.com>
2023-02-06 05:46:18 -0800

    Re-define getYearsSinceBirthdayAge()

--- src/js/intergalactic.js
+++ src/js/intergalactic.js
@@ -35,7 +35,45 @@ export default class Calculator{
   // for each planet in plantes, do this. 
   // Map.
 
-  getYearsSinceBirthdayAge(){
+  getYearsSinceBirthdayAge(){//planetName
+    //   let birthdayAge = this.birthdayAge;
+    //   let planets = this.planets;
+
     
+    //   const iCalculator = new Map(
+    //   [
+    //     ["Earth", 1],
+    //     ["Mercury", .24],
+    //     ["Venus", .62],
+    //     ["Mars", 1.88], 
+    //     ["Jupiter", 11.86]
+    //   ]
+    // );
+    // if(birthdayAge > 0){
+    //   return Number(birthdayAge - iCalculator.entries(planetName));
+    // }
+    // // iCalculator.entries()
+  
+      console.log('planets', planets);
+      console.log('this.planets', this.planets);
+      let returnedPlanets = {}
+  
+      let planetAge = {
+        earth: birthdayAge,
+        mercury: birthdayAge *.24,
+        venus: birthdayAge * .62,
+        mars: birthdayAge * 1.88,
+        jupiter: birthdayAge * 11.86
+      }
+      console.log('evaluated planetAge', planetAge)
+      // console.log('planet:', planet, 'age:', planetAge[planet])
+  
+      let planetsAgeArr = planets.forEach(planet => {
+        returnedPlanets = {...returnedPlanets, [planet]: planetAge[planet]}
+      })
+      console.log('returnedPlanets', returnedPlanets);
+  
+      return planetsAgeArr;
+
   }
 }
