ef29c5218746be0d407432ac9b45109bf34214cc
Vera Weikel <vera.weikel@gmail.com>
2023-02-06 05:36:09 -0800

    Re-add validator() addl or stmt.

--- __tests__/intergalactic.test.js
+++ __tests__/intergalactic.test.js
@@ -7,28 +7,16 @@ describe('Calculator', () => {
     expect(calculator.birthdayAge).toEqual(92);
   });
 
-  // test('Should account for validation of an undefined an inputted age', () => {
-  //   const calculator = new Calculator(undefined);
-  //   expect(calculator.givenAgeValidation()).toEqual(0);
-  // }); 
+  test('Should account for validation of an undefined an inputted age', () => {
+    const calculator = new Calculator(undefined);
+    expect(calculator.givenAgeValidation()).toEqual(0);
+  }); 
 
   test('Should test validation of an age is not equal to zero', () => {
   const calculator =  new Calculator();
   expect(calculator.givenAgeValidation()).toEqual(0);
   expect(calculator.birthdayAge).toEqual(0);
   });
-
-  // test('Should test validation of an age given an inputted age is not an integer', () => {
-  //   const calculator =  new Calculator(0);
-  //   expect(calculator.givenAgeValidation()).toEqual(0);
-  // });
-  
-  // // if NaN
-  // test('Should test validation of an age given an inputted age is not an integer', () => {
-  //   const calculator =  new Calculator("%");
-  //   expect(calculator.birthdayAge).toEqual(NaN);
-  //   expect(calculator.givenAgeValidation()).toEqual(NaN);
-  // });
 
   test('Should test Mercury age given an inputted age', () => {
     const calculator =  new Calculator(4);
@@ -55,7 +43,7 @@ describe('Calculator', () => {
     //   expect(calculator.getYearsSinceBirthdayAge()).toEqual(4, .96, 2.48, 7.52, 47.44);
     // });
     
-    // test('Should test: Lee', () => {
+    // test('Should test entry of each given parameter looped', () => {
     //   const calculator =  new Calculator(5, [ "earth", "mercury", "venus", "mars", "jupiter" ]);
     //   console.log('calculator', calculator);
     // // expect(calculator.getYearsSinceBirthdayAge()).toEqual(59.3, "jupiter");
