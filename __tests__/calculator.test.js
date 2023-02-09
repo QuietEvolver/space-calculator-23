456ab7c96979b88b4bb33c8f051147d0a3e7ad3b
Vera Weikel <vera.weikel@gmail.com>
2023-02-09 12:42:28 -0800

    Test if bday == undef.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -18,25 +18,11 @@ describe('Calculator', () => {
     expect(calculator.givenAgeValidation()).toEqual(0);
   }); 
   
-  // test('Should test validation of an age is not equal to zero', () => {
-  // const calculator =  new Calculator();
-  // expect(calculator.givenAgeValidation()).toEqual(0);
-  // expect(calculator.birthdayAge).toEqual(0);
-  // });
-
-  // test('should create an earth age', () => {
-  //   const calculator =  new Calculator(5);
-  //   expect(calculator.birthdayAge).toEqual(15);
-  // });
-
-  // test('should correctly say how many Earth years have passed since last birthday', () => {
-  //   calculator.earthAge(12);
-  //   expect(calculator.yearsSince).toEqual(15);
-  // })
-  // test('should correctly say how many Earth years until a persons future birthday', () => {
-  //   calculator.earthAge(5);
-  //   expect(calculator.yearsUntil).toEqual(13)
-  // })
+  test('Should test validation of an age is not equal to zero', () => {
+  const calculator =  new Calculator();
+  expect(calculator.givenAgeValidation()).toEqual(0);
+  expect(calculator.birthdayAge).toEqual(0);
+  });
 
   test('Should test Mercury age given an inputted age', () => {
     const calculator =  new Calculator(4);
