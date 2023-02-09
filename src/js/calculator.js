5be096f3dcb45918c98e9f6dd9a0394fab9a6840
Vera Weikel <vera.weikel@gmail.com>
2023-02-09 12:41:49 -0800

    Js if bday == undef.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -5,7 +5,7 @@ export default class Calculator{
 
   givenAgeValidation(){
     /* istanbul ignore else */ 
-    if(this.birthdayAge === NaN  || this.birthdayAge < 0 ){
+    if(this.birthdayAge === NaN  || this.birthdayAge < 0 || this.birthdayAge === undefined ){
       this.birthdayAge = 0;
       return this.birthdayAge; 
     } 
